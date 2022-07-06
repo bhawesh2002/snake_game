857e5d5da5a08a096ce8ef72365ec1e4429580b2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-06 18:34:06 +0530

    add images of snakes

--- snake_game.py
+++ snake_game.py
@@ -9,9 +9,13 @@ width,height = 600,600
 window = pygame.display.set_mode((width,height))
 caption = pygame.display.set_caption("!!!SNAKE!!!")
 white = (255,255,225)
+green_snake = pygame.image.load(os.path.join('Assets','green snake.png'))
+orange_snake = pygame.image.load(os.path.join('Assets','snake orange.png'))
 run = True
 while run:
     window.fill(white)
+    window.blit(green_snake,(400,400))
+    window.blit(orange_snake,(200,200))
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYUP and K_q):
