503b8436179d29bfb3ff11ca56fde82daaa4a73e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-06 18:05:33 +0530

    quit the game

--- snake_game.py
+++ snake_game.py
@@ -8,3 +8,9 @@ pygame.init()
 width,height = 600,600
 window = pygame.display.set_mode((width,height))
 caption = pygame.display.set_caption("!!!SNAKE!!!")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYUP and K_q):
+            pygame.quit()
+            sys.exit()
